305154ee7e109ed0ac3aea9dbdb696451f3b3423
Caio Benatti Moretti <caio.benatti@xebia.com>
2025-11-24 17:15:53 +0100

    image editor example

--- autogui/example.py
+++ autogui/example.py
@@ -1,76 +1,65 @@
 import streamlit as st
+import numpy as np
+from PIL import Image
 
 from autogui import autogui
 
-num = st.number_input("some input",0)
+st.set_page_config(layout="wide")
 
-#def pipeline(n: int) -> int:
-#    return True
-#
-#value_a = autogui("teste", like=pipeline, args=dict(n=num))
-#st.write(value_a)
+preview, pipeline = st.columns([0.7,0.3])
 
-def pipeline(n: int) -> (float,bool):
+preview.subheader("Image")
+tab_load, tab_proc, tab_orig = preview.tabs(["Upload", "Processed", "Original"])
+
+def load_image() -> np.ndarray:
+    """ {IO} """
+
+    loaded_img = autogui("Image load testing tes", init_prompt="upload file")
+    return loaded_img
+
+with tab_load:
+    img = load_image()
+
+
+pipeline.subheader("Pipeline")
+
+
+def proc_pipeline(img: np.ndarray) -> np.ndarray:
     """
-You are a coding assistant specialist in streamlit dashboards. Given a list of
-steps, your job is to identify each step and then generate code to accomplish
-such tasks sequentially. Provide UI elements for all parameters and/or inputs
-involved in every step.
+    {IO}
+    {VISUALIZATION}
 
-Provide only code and nothing else. Do not include markdown backticks.
+Make sure to organize every step of the pipeline in an individual streamlit
+expander. The image always must always go through every step of the pipeline
+and return the final result.
 
-The code you generate will be included in function `{FUNCTION_NAME}`, which
-takes `{INPUT_SCHEMA}` as input arguments and returns `{OUTPUT_SCHEMA}` as
-output. One output is the result itself, the other is whether the output is
-prime (no GUI for the latter). Make sure to define unique keys for every
-streamlit component. Never use streamlit sidebar. Never use any streamlit
-experimental feature. Never use streamlit caching.
-
-Each task must be individually detected and their UI and visualization elements
-must be organized, separately, into their respective streamlit expander for
-better user experience.
-
-The IO of tasks must be chained as a pipeline. The output of one task must be
-used as the input of the next, and the `{OUTPUT_SCHEMA}` is the final result
-to be provided as output.
-    """
-    val1, val2 = autogui("teste")
-    return val1,val2
-
-value, prime = pipeline(n=num)
-
-
-st.write(f"Output is {value}")
-if prime:
-    st.write(f"and {value} is prime")
-
-
-def teste(n:int) -> bool:
-    """
-You are a coding assistant specialist in streamlit dashboards. Given a list of
-steps, your job is to identify each step and then generate code to accomplish
-such tasks sequentially. Provide UI elements for all parameters and/or inputs
-involved in every step.
-
-Provide only code and nothing else. Do not include markdown backticks.
-
-The code you generate will be included in function `{FUNCTION_NAME}`, which
-takes `{INPUT_SCHEMA}` as input arguments and returns `{OUTPUT_SCHEMA}` as
-output. One output is the result itself, the other is whether the output is
-prime (no GUI for the latter). Make sure to define unique keys for every
-streamlit component. Never use streamlit sidebar. Never use any streamlit
-experimental feature. Never use streamlit caching.
-
-Each task must be individually detected and their UI and visualization elements
-must be organized, separately, into their respective streamlit expander for
-better user experience.
-
-The IO of tasks must be chained as a pipeline. The output of one task must be
-used as the input of the next, and the `{OUTPUT_SCHEMA}` is the final result
-to be provided as output.
+Always add, for each expander, a component to toggle whether to apply that step
+to the image. Never hide the components.
 
 """
-    iseven = autogui("Even odd")
-    return iseven
 
-st.write(teste(n=num))
+    proc_img = autogui("Pipeline", history=autogui.STATIC, init_prompt="Brightness and contrast\n\nblack and white\n\ncrop")
+    return proc_img
+
+def view_img(img: np.ndarray) -> None:
+    """ {IO} {VISUALIZATION} """
+
+    autogui("img view", init_prompt="display image")
+    return None
+
+
+
+
+if isinstance(img,np.ndarray):
+    proc = img.copy()
+
+    with pipeline:
+        proc = proc_pipeline(img=proc)
+
+    proc = proc if isinstance(proc,type(img)) else img
+
+    image = Image.fromarray(img)
+    tab_orig.image(Image.fromarray(img), caption="Original", use_container_width=False)
+
+    with tab_proc:
+        view_img(proc)
